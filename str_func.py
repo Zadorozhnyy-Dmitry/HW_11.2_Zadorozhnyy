44b16f0cb04e088d70bef3e09899c87a2de295d4
Dmitry Zadorozhnyy <zadorozhnyy_d_m@mail.ru>
2024-04-29 11:54:34 +0300

    modify str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def letters_up(user_str: str) -> str:
     """Функция заглавных букв"""
+    """Еще одна докстринга локально"""
     return user_str.upper()
+
+
+def letters_cap(user_srt: str) -> str:
+    """Функция заглавных первых букв"""
+    return user_srt.capitalize()
